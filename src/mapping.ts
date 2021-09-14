17cc719edb09fe635d4d96bcc158eee1874a46f3
RedBeard <redbeard.eth@protonmail.com>
2021-09-14 14:02:24 +1000

    add mloot

--- src/mapping.ts
+++ src/mapping.ts
@@ -2,8 +2,9 @@ import { Transfer as TransferEvent } from '../generated/Loot/Loot';
 import { Transfer as RealmTransferEvent } from '../generated/LootRealm/LootRealm';
 import { Transfer as TreasureTransferEvent } from '../generated/LootTreasure/LootTreasure';
 
-import { Bag, Transfer, Wallet, Realm, Treasure } from '../generated/schema';
+import { Bag, MLoot, Transfer, Wallet, Realm, Treasure } from '../generated/schema';
 import { Loot } from '../generated/Loot/Loot';
+import { LootMore } from '../generated/LootMore/LootMore';
 import { LootRealm } from '../generated/LootRealm/LootRealm';
 import { LootTreasure } from '../generated/LootTreasure/LootTreasure';
 
@@ -67,6 +68,70 @@ export function handleTransfer(event: TransferEvent): void {
   );
 
   transfer.bag = tokenId.toString();
+  transfer.from = fromWallet.id;
+  transfer.to = toWallet.id;
+  transfer.txHash = event.transaction.hash;
+  transfer.timestamp = event.block.timestamp;
+  transfer.save();
+}
+export function handleMLootTransfer(event: TransferEvent): void {
+  let fromAddress = event.params.from;
+  let toAddress = event.params.to;
+  let tokenId = event.params.tokenId;
+  let fromId = fromAddress.toHex();
+  let fromWallet = Wallet.load(fromId);
+
+  if (!fromWallet) {
+    fromWallet = new Wallet(fromId);
+    fromWallet.address = fromAddress;
+    fromWallet.joined = event.block.timestamp;
+    fromWallet.mLootHeld = BigInt.fromI32(0);
+    fromWallet.save();
+  } else {
+    if (!isZeroAddress(fromId)) {
+      fromWallet.mLootHeld = fromWallet.mLootHeld.minus(BigInt.fromI32(1));
+      fromWallet.save();
+    }
+  }
+
+  let toId = toAddress.toHex();
+  let toWallet = Wallet.load(toId);
+  if (!toWallet) {
+    toWallet = new Wallet(toId);
+    toWallet.address = toAddress;
+    toWallet.joined = event.block.timestamp;
+    toWallet.mLootHeld = BigInt.fromI32(1);
+    toWallet.save();
+  } else {
+    toWallet.mLootHeld = toWallet.mLootHeld.plus(BigInt.fromI32(1));
+    toWallet.save();
+  }
+
+  let mLoot = MLoot.load(tokenId.toString());
+  if (mLoot != null) {
+    mLoot.currentOwner = toWallet.id;
+    mLoot.save();
+  } else {
+    mLoot = new MLoot(tokenId.toString());
+    let contract = LootMore.bind(event.address);
+    mLoot.chest = contract.getChest(tokenId);
+    mLoot.foot = contract.getFoot(tokenId);
+    mLoot.hand = contract.getHand(tokenId);
+    mLoot.head = contract.getHead(tokenId);
+    mLoot.neck = contract.getNeck(tokenId);
+    mLoot.ring = contract.getRing(tokenId);
+    mLoot.waist = contract.getWaist(tokenId);
+    mLoot.weapon = contract.getWeapon(tokenId);
+    mLoot.currentOwner = toWallet.id;
+    mLoot.minted = event.block.timestamp;
+    mLoot.save();
+  }
+
+  let transfer = new Transfer(
+    event.transaction.hash.toHex() + '-' + event.logIndex.toString()
+  );
+
+  transfer.mLoot = tokenId.toString();
   transfer.from = fromWallet.id;
   transfer.to = toWallet.id;
   transfer.txHash = event.transaction.hash;
@@ -200,3 +265,4 @@ export function handleRealmTransfer(event: RealmTransferEvent): void {
 function isZeroAddress(string: string): boolean {
   return string == '0x0000000000000000000000000000000000000000';
 }
+
