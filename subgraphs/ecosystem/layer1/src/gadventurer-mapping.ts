481ba7ed96561d67b4e3924ed4c119f4ffec6da1
tenoxious <greg.choi+oxten@gmail.com>
2022-03-11 22:22:49 -0800

    Update GenesisAdventurer mapping to handle new V3 renderer, and GLR stats

--- subgraphs/ecosystem/layer1/src/gadventurer-mapping.ts
+++ subgraphs/ecosystem/layer1/src/gadventurer-mapping.ts
@@ -1,47 +1,160 @@
-import { GAdventurer } from '../generated/schema';
-import { Transfer as TransferEvent } from '../generated/LootMore/LootMore';
-import { GenesisAdventurer } from '../generated/GenesisProjectAdventurer/GenesisAdventurer';
-import { getTransfer, getWallets, isZeroAddress } from './utils';
+import { GAdventurer, Transfer } from "../generated/schema";
+import {
+  NameAdventurer,
+  NameLostMana,
+  ResurrectGACall,
+  GenesisAdventurer,
+  Transfer as TransferEvent
+} from "../generated/GenesisProjectAdventurer/GenesisAdventurer";
+import { getTransfer, getWallets, isZeroAddress } from "./utils";
+import { Address, BigInt } from "@graphprotocol/graph-ts";
+import {
+  getGAdventurerGreatness,
+  getGAdventurerLevel,
+  getGAdventurerRating
+} from "./glr-utils";
 
-import { BigInt } from '@graphprotocol/graph-ts';
+const V3_CONTRACT_START_TOKEN_ID = BigInt.fromI32(480);
 
 export function handleTransfer(event: TransferEvent): void {
   let tokenId = event.params.tokenId;
-    let wallets = getWallets(event.params.from, event.params.to, event);
+  let wallets = getWallets(event.params.from, event.params.to, event);
 
-
-  if(!isZeroAddress(wallets.fromWallet.id)) {
-    wallets.fromWallet.gAdventurersHeld = wallets.fromWallet.gAdventurersHeld.minus(BigInt.fromI32(1))
+  if (!isZeroAddress(wallets.fromWallet.id)) {
+    wallets.fromWallet.gAdventurersHeld = wallets.fromWallet.gAdventurersHeld.minus(
+      BigInt.fromI32(1)
+    );
   }
-  wallets.fromWallet.save()
+  wallets.fromWallet.save();
 
-  wallets.toWallet.gAdventurersHeld = wallets.toWallet.gAdventurersHeld.plus(BigInt.fromI32(1))
-  wallets.toWallet.save()
+  wallets.toWallet.gAdventurersHeld = wallets.toWallet.gAdventurersHeld.plus(
+    BigInt.fromI32(1)
+  );
+  wallets.toWallet.save();
 
   let gAdventurer = GAdventurer.load(tokenId.toString());
+  if (gAdventurer != null) {
+    gAdventurer.currentOwner = wallets.toWallet.id;
+    gAdventurer.save();
+  } else {
+    gAdventurer = new GAdventurer(tokenId.toString());
+    let contract = GenesisAdventurer.bind(event.address);
+    // Loot Token Ids aren't availble yet on inital GA Transfer
+    // resurrectGA call handler will fill in lootIds
+    updateGAdventurerWithLootTokenIds(gAdventurer, [], contract);
+    gAdventurer.currentOwner = wallets.toWallet.id;
+    gAdventurer.minted = event.block.timestamp;
+    gAdventurer.save();
+
+    // New V3 contract updates renderer
+    if (tokenId.equals(V3_CONTRACT_START_TOKEN_ID)) {
+      refreshAdventurersBeforeTokenId(
+        V3_CONTRACT_START_TOKEN_ID,
+        event.address
+      );
+    }
+  }
+
+  let transfer = getTransfer(event, wallets);
+  transfer.gAdventurer = tokenId.toString();
+  transfer.save();
+}
+
+export function handleResurrectGA(call: ResurrectGACall): void {
+  const transfer = Transfer.load(call.transaction.hash.toHex() + "0");
+  if (transfer != null && transfer.gAdventurer != null) {
+    const gAdventurer = GAdventurer.load(transfer.gAdventurer as string);
     if (gAdventurer != null) {
-      gAdventurer.currentOwner = wallets.toWallet.id;
-      gAdventurer.save();
-    } else {
-      gAdventurer = new GAdventurer(tokenId.toString());
-      let contract = GenesisAdventurer.bind(event.address);
-      gAdventurer.chest = contract.getChest(tokenId);
-      gAdventurer.foot = contract.getFoot(tokenId);
-      gAdventurer.hand = contract.getHand(tokenId);
-      gAdventurer.head = contract.getHead(tokenId);
-      gAdventurer.neck = contract.getNeck(tokenId);
-      gAdventurer.ring = contract.getRing(tokenId);
-      gAdventurer.waist = contract.getWaist(tokenId);
-      gAdventurer.weapon = contract.getWeapon(tokenId);
-      gAdventurer.order = contract.getOrder(tokenId);
-      gAdventurer.orderColor = contract.getOrderColor(tokenId);
-      gAdventurer.orderCount = contract.getOrder(tokenId);
-      gAdventurer.currentOwner = wallets.toWallet.id;
-      gAdventurer.minted = event.block.timestamp;
+      let contract = GenesisAdventurer.bind(call.to);
+      updateGAdventurerWithLootTokenIds(
+        gAdventurer,
+        [
+          call.inputs.waistTokenId,
+          call.inputs.chestTokenId,
+          call.inputs.headTokenId,
+          call.inputs.waistTokenId,
+          call.inputs.footTokenId,
+          call.inputs.handTokenId,
+          call.inputs.neckTokenId,
+          call.inputs.ringTokenId
+        ],
+        contract
+      );
       gAdventurer.save();
     }
-  
-    let transfer = getTransfer(event, wallets)  
-    transfer.gAdventurer = tokenId.toString();
-    transfer.save();
   }
+}
+
+export function handleNameAdventurer(event: NameAdventurer): void {
+  refreshGAdventurerByTokenId(event.params.tokenId, event.address);
+}
+
+export function handleNameLostMana(event: NameLostMana): void {
+  refreshGAdventurerByTokenId(event.params.tokenId, event.address);
+}
+
+function refreshAdventurersBeforeTokenId(
+  tokenId: BigInt,
+  contractAddress: Address
+): void {
+  if (tokenId.lt(BigInt.fromI32(1))) {
+    return;
+  }
+  for (let i = tokenId.toI32() - 1; i > 0; i--) {
+    refreshGAdventurerByTokenId(BigInt.fromI32(i), contractAddress);
+  }
+}
+
+function refreshGAdventurerByTokenId(
+  tokenId: BigInt,
+  contractAddress: Address
+): void {
+  let adventurer = GAdventurer.load(tokenId.toString());
+  if (adventurer) {
+    let contract = GenesisAdventurer.bind(contractAddress);
+    updateGAdventurerWithLootTokenIds(
+      adventurer,
+      contract.getLootTokenIds(tokenId),
+      contract
+    );
+    adventurer.save();
+  }
+}
+
+export function updateGAdventurerWithLootTokenIds(
+  gAdventurer: GAdventurer,
+  lootTokenIds: BigInt[],
+  contract: GenesisAdventurer
+): void {
+  const tokenId = BigInt.fromString(gAdventurer.id);
+  gAdventurer.weapon = contract.getWeapon(tokenId);
+  gAdventurer.chest = contract.getChest(tokenId);
+  gAdventurer.head = contract.getHead(tokenId);
+  gAdventurer.waist = contract.getWaist(tokenId);
+  gAdventurer.foot = contract.getFoot(tokenId);
+  gAdventurer.hand = contract.getHand(tokenId);
+  gAdventurer.neck = contract.getNeck(tokenId);
+  gAdventurer.ring = contract.getRing(tokenId);
+  gAdventurer.order = contract.getOrder(tokenId);
+  gAdventurer.orderColor = contract.getOrderColor(tokenId);
+  gAdventurer.orderCount = contract.getOrderCount(tokenId);
+  gAdventurer.tokenURI = contract.tokenURI(tokenId);
+
+  if (lootTokenIds.length == 0) {
+    gAdventurer.lootTokenIds = [];
+    gAdventurer.bagGreatness = 0;
+    gAdventurer.bagLevel = 0;
+    gAdventurer.bagRating = 0;
+  } else {
+    gAdventurer.lootTokenIds = lootTokenIds.map(function(
+      tokenId: BigInt,
+      index: i32,
+      array: BigInt[]
+    ): i32 {
+      return tokenId.toI32();
+    });
+    gAdventurer.bagGreatness = getGAdventurerGreatness(gAdventurer);
+    gAdventurer.bagLevel = getGAdventurerLevel(gAdventurer);
+    gAdventurer.bagRating = getGAdventurerRating(gAdventurer);
+  }
+}
--- subgraphs/ecosystem/layer1/src/mana-mapping.ts
+++ subgraphs/ecosystem/layer1/src/mana-mapping.ts
@@ -1,50 +1,103 @@
-import { Transfer as TransferEvent } from '../generated/GenesisProjectMana/GenesisMana';
-import { getTransfer, getWallets, isZeroAddress } from './utils';
-
-import { Bag, Mana } from '../generated/schema';
-import { GenesisMana } from '../generated/GenesisProjectMana/GenesisMana';
-import { BigInt } from '@graphprotocol/graph-ts';
+import { getTransfer, getWallets, isZeroAddress } from "./utils";
+import { Bag, GAdventurer, Mana, Transfer } from "../generated/schema";
+import { GenesisMana,  Transfer as TransferEvent  } from "../generated/GenesisProjectMana/GenesisMana";
+import { Address, BigInt } from "@graphprotocol/graph-ts";
+import { updateGAdventurerWithLootTokenIds } from "./gadventurer-mapping";
+import { GenesisAdventurer } from "../generated/GenesisProjectAdventurer/GenesisAdventurer";
 
 export function handleTransfer(event: TransferEvent): void {
   let tokenId = event.params.tokenId;
   let wallets = getWallets(event.params.from, event.params.to, event);
 
-  if(!isZeroAddress(wallets.fromWallet.id)) {
-    wallets.fromWallet.manasHeld = wallets.fromWallet.manasHeld.minus(BigInt.fromI32(1))
+  if (!isZeroAddress(wallets.fromWallet.id)) {
+    wallets.fromWallet.manasHeld = wallets.fromWallet.manasHeld.minus(
+      BigInt.fromI32(1)
+    );
   }
-  wallets.fromWallet.save()
+  wallets.fromWallet.save();
 
-  wallets.toWallet.manasHeld = wallets.toWallet.manasHeld.plus(BigInt.fromI32(1))
-  wallets.toWallet.save()
-  
+  wallets.toWallet.manasHeld = wallets.toWallet.manasHeld.plus(
+    BigInt.fromI32(1)
+  );
+  wallets.toWallet.save();
+
   let lootTokenId = "";
   let mana = Mana.load(tokenId.toString());
-    if (mana != null) {
-      mana.currentOwner = wallets.toWallet.id;
-      mana.save();
-    } else {
-      mana = new Mana(tokenId.toString());
-      let contract = GenesisMana.bind(event.address);
-      let manaDetails = contract.detailsByToken(tokenId);
-      lootTokenId =  manaDetails.value0.toString();
-  
-      mana.lootTokenId = lootTokenId
-      mana.itemName = manaDetails.value1;
-      mana.suffixId = manaDetails.value2;
-      mana.inventoryId = manaDetails.value3;
-      mana.currentOwner = wallets.toWallet.id;
-      mana.minted = event.block.timestamp;
-      mana.save();
-    }
-    let bag = Bag.load(lootTokenId);
-    if (bag != null) {
-      if (bag.manasClaimed)
-        bag.manasClaimed = bag.manasClaimed.plus(BigInt.fromI32(1));
-      bag.save();
-    }
-  
-    let transfer = getTransfer(event, wallets)  
-    transfer.mana = tokenId.toString();
-    transfer.save()
-  
+  if (mana != null) {
+    mana.currentOwner = wallets.toWallet.id;
+    mana.save();
+  } else {
+    mana = new Mana(tokenId.toString());
+    let contract = GenesisMana.bind(event.address);
+    let manaDetails = contract.detailsByToken(tokenId);
+    lootTokenId = manaDetails.value0.toString();
+
+    mana.lootTokenId = lootTokenId;
+    mana.itemName = manaDetails.value1;
+    mana.suffixId = manaDetails.value2;
+    mana.inventoryId = manaDetails.value3;
+    mana.currentOwner = wallets.toWallet.id;
+    mana.minted = event.block.timestamp;
+    mana.save();
   }
+  let bag = Bag.load(lootTokenId);
+  if (bag != null) {
+    if (bag.manasClaimed)
+      bag.manasClaimed = bag.manasClaimed.plus(BigInt.fromI32(1));
+    bag.save();
+  }
+
+  let transfer = getTransfer(event, wallets);
+  transfer.mana = tokenId.toString();
+  transfer.save();
+
+  updateGAdventurerIfSummoned(event);
+}
+
+function updateGAdventurerIfSummoned(event: TransferEvent): void {
+  // If GA summoning, last transfer will be the ring at logIndex 16
+  if (event.logIndex.toString() != "16") {
+    return;
+  }
+
+  // GAdventurer is the first transfer in transaction
+  const transaction = event.transaction.hash.toHex();
+  const gaTransfer = Transfer.load(transaction + "-0");
+
+  if (gaTransfer == null || gaTransfer.gAdventurer == null) {
+    return;
+  }
+
+  const tokenId = gaTransfer.gAdventurer as string;
+  const gAdventurer = GAdventurer.load(tokenId);
+  if (gAdventurer == null) {
+    return;
+  }
+  updateGAdventurerWithLootTokenIds(
+    gAdventurer as GAdventurer,
+    [
+      getLootIdByManaTransferId(transaction + "-2"), // Weapon
+      getLootIdByManaTransferId(transaction + "-4"), // Chest
+      getLootIdByManaTransferId(transaction + "-6"), // Head
+      getLootIdByManaTransferId(transaction + "-8"), // Waist
+      getLootIdByManaTransferId(transaction + "-10"), // Foot
+      getLootIdByManaTransferId(transaction + "-12"), // Hand
+      getLootIdByManaTransferId(transaction + "-14"), // Neck
+      getLootIdByManaTransferId(transaction + "-16") // Ring
+    ],
+    GenesisAdventurer.bind(event.transaction.to as Address)
+  );
+  gAdventurer.save();
+}
+
+function getLootIdByManaTransferId(transferId: string): BigInt {
+  const manaTransfer = Transfer.load(transferId);
+  if (manaTransfer == null || manaTransfer.mana == null) {
+    return BigInt.fromI32(0);
+  }
+  const mana = Mana.load(manaTransfer.mana as string);
+  if (mana == null || mana.lootTokenId == null) {
+    return BigInt.fromI32(0);
+  }
+  return BigInt.fromString(mana.lootTokenId);
+}
